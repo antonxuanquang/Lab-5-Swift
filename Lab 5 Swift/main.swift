a9c4ea09ce4b620a124e51d8418365398e8ce786
Nguyen Quang <quangnguyen787@tucl331016.cos.tcu.edu>
2016-04-05 22:12:48 -0500

    Initial Commit

---
+++ Lab 5 Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lab 5 Swift
+//
+//  Created by Nguyen, Quang on 4/5/16.
+//  Copyright © 2016 Nguyen, Quang. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
